e15b4da84665cdedeef69b48e8dc3118f5c54750
Justin Cheng <jucheng.ca@gmail.com>
2024-02-26 19:34:24 -0800

    Added vehicle data in list

--- client/src/components/Vehicle/Info.jsx
+++ client/src/components/Vehicle/Info.jsx
@@ -3,16 +3,21 @@ import axios from 'axios';
 
 const Info = ({vehicleData}) => {
 
+  console.log(vehicleData);
   return (
     <div>
       <div className="specs">
       <ul>
       <h2 className="text-xl font-bold mb-0">{vehicleData.year} {vehicleData.make} {vehicleData.model} {vehicleData.trim}</h2>
           <p className="text-sm font-bold mb-0">{vehicleData.nickname}</p>
+          <p className="text-sm mb-0">{vehicleData.color}</p>
+          <p className="text-sm mb-0">{vehicleData.motor}</p>
+          <p className="text-sm mb-0">{vehicleData.reg_month}</p>
           <p className="text-sm mb-0">{vehicleData.vin}</p>
           <p className="text-sm mb-0">{vehicleData.mileage} mi</p>
       </ul>
     </div>
+    <div></div>
       <div className="info">
         <ul>
           <li>Maintenance</li>
